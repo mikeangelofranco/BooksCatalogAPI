bff26d542532bddfec85f80c404fbc1969e14b6c
mikeangelofranco <99171166+mikeangelofranco@users.noreply.github.com>
2022-02-16 10:55:11 +0800

    Add files via upload
    Initial commit.

---
+++ BooksCatalog/Controllers/BooksController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace BooksCatalog.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BooksController : ControllerBase
+    {
+        private readonly ILogger<BooksController> _logger;
+
+        public BooksController(ILogger<BooksController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+
+        }
+    }
+}
